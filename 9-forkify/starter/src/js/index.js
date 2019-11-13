9b3968a531748127aba601f232f04d58b4c15909
Pink Moonlight <kate.cherie25@gmail.com>
2019-11-12 16:57:28 -0800

    added UI for recipe and logic for ingredient count and servings

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -12,40 +12,139 @@ console.log(`Using imported functions ${searchView.add(searchView.ID, 2)} and ${
 */
 
 import Search from './models/Search';
+import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
-import {elements} from './views/base';
+import * as recipeView from './views/recipeView';
+import {elements, renderLoader, clearLoader} from './views/base';
 
 /** Global state of the app 
  * - Search object
  * - current recipe object
- * - shipping list object
- * -liked recipes 
+ * - shopping list object
+ * - liked recipes 
  * */
 const state = {};
 
+/**
+ * SEARCH CONTROLLER
+ */
 const controlSearch = async () => {
     // 1. Get query from view
     const query = searchView.getInput();
-    console.log(query);
+    //console.log(query);
 
     if (query) {
         // 2. New Search object and add to state
         state.search = new Search(query); 
-
+        
         //3. prepare UI results
         searchView.clearInput();
         searchView.clearResults();
+        renderLoader(elements.searchRes);
 
-        //4. search for recipies 
-         await state.search.getResults();
+        try { 
+            //4. search for recipies 
+            await state.search.getResults();
 
-        //5. render results on UI
-        searchView.renderResults(state.search.result);
+            //5. render results on UI
+            clearLoader();
+            searchView.renderResults(state.search.result);           
+        } catch (error) {
+            alert('Something went wront with the search...');
+            clearLoader();
+        }
+
     };
 };
+
 
 elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 });
 
+/*
+//TESTING
+window.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+*/
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10); //base 10 means 1 - 10 (2 would be binary 0 or 1)
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+});
+
+/**
+ * RECIPE CONTROLLER
+ * 
+ /* 
+ USED FOR INITIAL TESTING
+ const r = new Recipe(47746);
+r.getRecipe();
+console.log(r);
+*/
+
+const controlRecipe = async () => {
+    //Get ID from url
+    const id = window.location.hash.replace('#', '');
+    console.log(id);
+
+    if (id) {
+        // Prepare UI for changes
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
+
+        //Hightlight Selected serach item
+        if (state.search) searchView.highlightSelected(id);
+
+        // Create new recipe object
+        state.recipe = new Recipe(id);
+
+        //TESTING
+        //window.r = state.recipe;
+
+        try {
+               // Get recipe data and parse ingredients
+        await state.recipe.getRecipe();
+        console.log(state.recipe.ingredients);
+        state.recipe.parseIngredients();
+        // Calculate servings and time
+        state.recipe.calcTime();
+        state.recipe.calcServings();
+        // Render recipe
+        clearLoader();
+        recipeView.renderRecipe(state.recipe);
+        console.log(state.recipe);
+        } catch (error) {
+            alert('Error processing recipe!');
+            console.log(error);
+        }
+      
+
+    }
+};
+
+
+
+//way to handle multiple events for same function
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+// Handling recipe button clicks
+elements.recipe.addEventListener('click', e => {
+    if (e.target.matches('.btn-decrease, .btn-decrease *')) { //  * means any child elements of btn-decrease
+        // decrease button is clicked
+        if (state.recipe.servings > 1) {
+            state.recipe.updateServings('dec');
+        }
+        
+    } else if (e.target.matches('.btn-increase, .btn-increase *')) {
+        // increase button is clicked
+    }   state.recipe.updateServings('inc');
+    console.log(state.recipe);
+    });
--- 9-forkify/starter/src/js/models/Search.js
+++ 9-forkify/starter/src/js/models/Search.js
@@ -7,7 +7,7 @@ export default class Search {
     async getResults() {
         //const key = 'key'; if using a key and would add in as a parameter below like query
         try {
-            const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${this.query}`);  //ajax call returns a promise
+            const res = await  axios(`https://forkify-api.herokuapp.com/api/search?q=${this.query}`);  //ajax call returns a promise
         this.result = res.data.recipes;
         //console.log(this.result);
         } catch (error) {
